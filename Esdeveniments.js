46b14455be47c2e3807fcc64830248fad3122f6f
didac2003 <didac.gros@institutvidreres.cat>
2021-12-13 09:36:46 +0100

    ex2

--- Esdeveniments.js
+++ Esdeveniments.js
@@ -1,1 +1,17 @@
 //Ex1
+/*
+function sfondo(color) {
+    document.bgColor = color;
+}
+*/
+
+//Ex2
+
+document.onkeydown=function(e){
+    document.getElementById("lletra").innerHTML=e.key;
+}
+
+document.onkeyup=function(){
+    document.getElementById("lletra").innerHTML=" "
+}
+*/
